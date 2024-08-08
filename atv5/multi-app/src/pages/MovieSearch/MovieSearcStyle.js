396d30beef5dbebe89e8aa76435b5ea70f1f0be5
Samuel Deotti <samuel_deotti@hotmail.com>
2024-08-08 13:58:06 -0300

    refactoring movie engine

---
+++ atv5/multi-app/src/pages/MovieSearch/MovieSearcStyle.js
@@ -0,0 +1,100 @@
+import styled from 'styled-components'; // Importa styled-components para estilizar os componentes
+
+// Define o estilo do container principal
+export const MovieContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 40px;
+  background: #fff;
+  border-radius: 15px;
+  box-shadow: 0 6px 12px rgba(0, 0, 0, 0.2);
+  max-width: 800px;
+  margin: 50px auto;
+`;
+
+// Define o estilo do título
+export const MovieTitle = styled.h2`
+  color: #333;
+  margin-bottom: 20px;
+  font-size: 24px;
+  text-align: center;
+`;
+
+// Define o estilo do campo de entrada
+export const MovieInput = styled.input`
+  margin-bottom: 20px;
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  width: 100%;
+  box-shadow: inset 0 2px 4px rgba(0, 0, 0, 0.1);
+  font-size: 16px;
+  transition: border-color 0.3s;
+
+  &:focus {
+    border-color: #007bff;
+    outline: none;
+  }
+`;
+
+// Define o estilo do botão
+export const MovieButton = styled.button`
+  padding: 12px 20px;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 16px;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: #0056b3;
+  }
+`;
+
+// Define o estilo do container dos filmes
+export const MoviesContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin-top: 20px;
+  max-height: 500px; /* Ajuste a altura máxima conforme necessário */
+  overflow-y: auto; /* Adiciona rolagem vertical se necessário */
+  width: 100%;
+`;
+
+// Define o estilo do cartão de filme
+export const MovieCard = styled.div`
+  background: #f9f9f9;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  margin: 10px;
+  padding: 20px;
+  width: 180px; /* Ajuste a largura conforme necessário */
+  text-align: center;
+  transition: transform 0.3s, box-shadow 0.3s;
+
+  &:hover {
+    transform: scale(1.05);
+    box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2);
+  }
+
+  img {
+    border-radius: 10px;
+    max-width: 100%; /* Ajusta o tamanho da imagem para caber dentro do cartão */
+    height: auto;
+    margin-bottom: 10px;
+  }
+
+  h3 {
+    font-size: 18px;
+    margin: 10px 0;
+  }
+
+  p {
+    font-size: 14px;
+    color: #555;
+  }
+`;
